c82927f68612ee48d0b0e0c7c524a3e45db60a1b
jtrejosb <jtrejosb@gmail.com>
2017-09-07 01:46:30 -0500

    Level Selection Values

---
+++ src/com/java/utils/Level.java
@@ -0,0 +1,46 @@
+package com.java.utils;
+
+/**
+* Engineered and developed by Jhonny Trejos Barrios.
+* Technology: Java.
+* Version: Java Development Kit 1.8.0_31, Standard Edition.
+* Development Environment: Sublime Text 3 [build 3126].
+* Date: 24/08/17, Time: 18:42:53.
+*
+* Additional Info.
+*
+* Source Code Target Or Details:
+*
+*     [ Level Selection Values ]
+*
+* Licenses: GNU GPL v3.0, Eclipse Public License 1.0, Personal for non-commercial purposes.
+* Developer Contact: jtrejosb@live.com || jtrejosb@gmail.com || jtrejosb@icloud.com
+* GitHub.com/jtrejosb
+*/
+
+public class Level {
+  public static int width, height, mines;
+
+  public static void setSize( int newWidth, int newHeight ) {
+    width = newWidth;
+    height = newHeight;
+  }
+
+  public static void setMines( int newMines ) {
+    mines = newMines;
+  }
+
+  public static int getWidth() {
+    return width;
+  }
+
+  public static int getHeight() {
+    return height;
+  }
+
+  public static int getMines() {
+    // return size == 9 ? 10 : size == 16 ? 40 : 99;
+    // return width <= 9 ? 10 : width <= 16 ? 40 : 99;
+    return mines;
+  }
+}
